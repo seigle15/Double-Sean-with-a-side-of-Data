82180f967451bd21814307b71ce3cd11d503abe4
AustinStewart95 <apstewart1210@yahoo.com>
2018-02-26 11:30:19 -0800

    Add files via upload

---
+++ dbmanager.h
@@ -0,0 +1,39 @@
+#ifndef DBMANAGER_H
+#define DBMANAGER_H
+
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QSqlDriver>
+#include <QDebug>
+#include <QFile>
+#include <QTextStream>
+#include <vector>
+#include <string>
+
+#include "restaurant.h"
+
+/**
+ * @brief The DBManager class
+ * Manages the database by allowing the manipulation of data within the database.
+ * Uses singleton desgin pattern.
+ */
+
+class DBManager
+{
+public:
+    static DBManager* getInstance();
+    void uploadFileToDatabase(const QString& filePath);
+    void databaseToRestaurants();
+    std::vector<Restaurant> getRestaurants();
+    void testDB();
+
+private:
+    static DBManager* database;
+    DBManager(const QString& filePath);
+    QSqlDatabase restaurantDatabase;
+    std::vector<Restaurant> restaurants;
+};
+
+#endif // DBMANAGER_H
---
+++ mainwindow.cpp
@@ -0,0 +1,170 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "qmessagebox.h"
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+//    connect(ui->restaurantListWidget, SIGNAL(itemDoubleClicked(QListWidgetItem*)),
+//            this, SLOT(on_restaurantListWidget_itemActivated(QListWidgetItem*)));
+
+    listRestaurants();
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_Login_clicked()
+{
+    QString username = ui->lineEdit_Username->text();
+    QString password = ui->lineEdit_Password->text();
+
+    if(username == "test" && password == "test")
+    {
+        AdminWindow = new Admin();
+        AdminWindow->show();
+
+        this->close();    }
+    else
+    {
+        QMessageBox::warning(this,"Login", "Username and password is not correct");
+    }
+}
+
+void MainWindow::listRestaurants()
+{
+    std::vector<Restaurant> myRestaurants(DBManager::getInstance()->getRestaurants());
+    for(int i = 0; i < myRestaurants.size(); i++)
+    {
+        ui->restaurantListWidget->addItem(myRestaurants.at(i).getName());
+    }
+}
+
+void MainWindow::on_addItemButton_clicked()
+{
+    qDebug() << "button pushed";
+    std::vector<Restaurant> myRestaurants(DBManager::getInstance()->getRestaurants());
+    QString foodName = ui->itemNameToRemove_lineEdit->text();
+
+    if(foodName == NULL)
+    {
+        qDebug() << "ERROR----Must select item to be added to cart.";
+    }
+    else
+    {
+        for(int i = 0; i < myRestaurants.size(); i++)
+        {
+            for(int j = 0; j < myRestaurants.at(i).getMenu().size(); j++)
+            {
+                if(myRestaurants.at(i).getMenu().at(j).getItemName() == foodName)
+                {
+                    //--------------------------------------
+                    // This is where the error is occuring.
+                    //--------------------------------------
+                        qDebug() << "added item to shopping cart.";
+                        shoppingCart.push_back(foodName);
+                        double price = myRestaurants.at(i).getMenu().at(j).getPrice();
+                        cartTotal += price;
+                        QString cartTotalStr = QString::number(cartTotal);
+                        ui->displayTotalLine->setText(cartTotalStr);
+                        //qDebug() << cartTotal;
+                }
+
+            }
+        }
+    }
+
+    qDebug() << "---Shopping Cart List---";
+    for(int i = 0; i < shoppingCart.size(); i++)
+    {
+        qDebug() << "food item #" << i + 1 << ": " << shoppingCart[i];
+    }
+    qDebug() << "------------------------";
+
+}
+
+void MainWindow::on_removeItemButton_clicked()
+{
+    qDebug() << "button pushed";
+    if(shoppingCart.size() == 0)
+    {
+        qDebug() << "ERROR---There are no items in the shopping cart.";
+        cartTotal = 0.00;
+    }
+    else
+    {
+        std::vector<Restaurant> myRestaurants(DBManager::getInstance()->getRestaurants());
+        QString foodName = shoppingCart.at(shoppingCart.size() - 1);
+        for(int i = 0; i < myRestaurants.size(); i++)
+        {
+            for(int j = 0; j < myRestaurants.at(i).getMenu().size(); j++)
+            {
+                if(myRestaurants.at(i).getMenu().at(j).getItemName() == foodName)
+                {
+                    //--------------------------------------
+                    // This is where the error is occuring.
+                    //--------------------------------------
+                        qDebug() << "removed item from shopping cart.";
+                        shoppingCart.pop_back();
+                        double price = myRestaurants.at(i).getMenu().at(j).getPrice();
+                        cartTotal -= price;
+                        QString cartTotalStr = QString::number(cartTotal);
+                        ui->displayTotalLine->setText(cartTotalStr);
+                        //qDebug() << cartTotal;
+                }
+
+            }
+        }
+    }
+
+    qDebug() << "---Shopping Cart List---";
+    for(int i = 0; i < shoppingCart.size(); i++)
+    {
+        qDebug() << "food item #" << i + 1 << ": " << shoppingCart[i];
+    }
+    qDebug() << "------------------------";
+}
+
+void MainWindow::on_menuListWidget_itemActivated(QListWidgetItem* item)
+{
+//    qDebug() << "Item clicked";
+    ui->itemNameToRemove_lineEdit->setText(item->text());
+    std::vector<Restaurant> myRestaurants(DBManager::getInstance()->getRestaurants());
+    for(int i = 0; i < myRestaurants.size(); i++)
+    {
+        for(int j = 0; j < myRestaurants.at(i).getMenu().size(); j++)
+        {
+            if(myRestaurants.at(i).getMenu().at(j).getItemName() == item->text())
+            {
+                double price = myRestaurants.at(i).getMenu().at(j).getPrice();
+                QString priceStr = QString::number(price);
+                ui->itemPrice_lineEdit->setText(priceStr);
+            }
+        }
+    }
+}
+
+void MainWindow::on_restaurantListWidget_itemActivated(QListWidgetItem* item)
+{
+    ui->menuListWidget->clear();
+    ui->itemPrice_lineEdit->clear();
+    ui->itemNameToRemove_lineEdit->clear();
+    std::vector<Restaurant> myRestaurants(DBManager::getInstance()->getRestaurants());
+    for(int i = 0; i < myRestaurants.size(); i++)
+    {
+        if(item->text() == myRestaurants.at(i).getName())
+        {
+            for(int j = 0; j < myRestaurants.at(i).getMenuSize(); j++)
+            {
+                QString itemName = myRestaurants.at(i).getMenu().at(j).getItemName();
+                ui->menuListWidget->addItem(itemName);
+            }
+        }
+    }
+}
