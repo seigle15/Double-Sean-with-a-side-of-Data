88f3d4e69df3b5c3ac9a11b6c49ba3f435517d31
Sean O'Hearn <sohearn1@saddleback.edu>
2018-03-28 14:03:34 -0700

    More doxygen

--- DSSDWorkspace/DSSDFastFoodProject/admin.h
+++ DSSDWorkspace/DSSDFastFoodProject/admin.h
@@ -21,21 +21,45 @@ public:
     ~Admin();
 
 private slots:
+    /**
+     * @brief on_pushButton_Logout_clicked
+     */
     void on_pushButton_Logout_clicked();
+    /**
+     * @brief listRestaurants
+     */
     void listRestaurants();
+    /**
+     * @brief on_itemsListWidget_itemActivated
+     */
     void on_itemsListWidget_itemActivated(QListWidgetItem*);
+    /**
+     * @brief on_priceListWidget_itemActivated
+     */
     void on_priceListWidget_itemActivated(QListWidgetItem*);
+    /**
+     * @brief on_restaurantListWidget_itemActivated
+     */
     void on_restaurantListWidget_itemActivated(QListWidgetItem*);
-
-
+    /**
+     * @brief on_pushButton_RemoveRestaurant_clicked
+     */
     void on_pushButton_RemoveRestaurant_clicked();
-
+    /**
+     * @brief on_pushButton_RemoveMenuItem_clicked
+     */
     void on_pushButton_RemoveMenuItem_clicked();
-
+    /**
+     * @brief on_pushButton_AddFromFile_clicked
+     */
     void on_pushButton_AddFromFile_clicked();
-
+    /**
+     * @brief on_pushButton_AddMenuItem_clicked
+     */
     void on_pushButton_AddMenuItem_clicked();
-
+    /**
+     * @brief on_pushButton_ChangePrice_clicked
+     */
     void on_pushButton_ChangePrice_clicked();
 
 private:
--- DSSDWorkspace/DSSDFastFoodProject/mainwindow.h
+++ DSSDWorkspace/DSSDFastFoodProject/mainwindow.h
@@ -25,58 +25,176 @@ public:
     ~MainWindow();
 
 private slots:
-
+    /**
+     * @brief on_pushButton_Login_clicked
+     */
     void on_pushButton_Login_clicked();
+    /**
+     * @brief on_restaurantListWidget_itemActivated
+     */
     void on_restaurantListWidget_itemActivated(QListWidgetItem*);
+    /**
+     * @brief on_addItemButton_clicked
+     */
     void on_addItemButton_clicked();
+    /**
+     * @brief addToCart
+     * @param foodName
+     * @param restaurantName
+     * @return  MenuItem
+     */
     MenuItem addToCart(QString foodName, QString restaurantName);
+    /**
+     * @brief on_removeItemButton_clicked
+     */
     void on_removeItemButton_clicked();
+    /**
+     * @brief on_menuAndTripListWidget_itemActivated
+     */
     void on_menuAndTripListWidget_itemActivated(QListWidgetItem*);
+    /**
+     * @brief listRestaurants
+     */
     void listRestaurants();
+    /**
+     * @brief listNormalRestView
+     */
     void listNormalRestView();
+    /**
+     * @brief on_AddRestaurant_clicked
+     */
     void on_AddRestaurant_clicked();
+    /**
+     * @brief isDuplicateRestaurant
+     * @param restaurantName
+     * @return bool
+     */
     bool isDuplicateRestaurant(QString restaurantName);
+    /**
+     * @brief addToMyTrip
+     * @param toAdd
+     */
     void addToMyTrip(Restaurant toAdd);
+    /**
+     * @brief isInCart
+     * @param itemToDelete
+     * @return bool
+     */
     bool isInCart(MenuItem itemToDelete);
-
-
+    /**
+     * @brief on_Remove_clicked
+     */
     void on_Remove_clicked();
+    /**
+     * @brief removeFromMyTrip
+     * @param RestToDelete
+     */
     void removeFromMyTrip(Restaurant RestToDelete);
+    /**
+     * @brief isOnMyTrip
+     * @param restToDelete
+     * @return bool
+     */
     bool isOnMyTrip(Restaurant restToDelete);
+    /**
+     * @brief on_viewTripButton_clicked
+     */
     void on_viewTripButton_clicked();
+    /**
+     * @brief listMyTrip
+     */
     void listMyTrip();
-
+    /**
+     * @brief on_restaurantListWidget_clicked
+     * @param index
+     */
     void on_restaurantListWidget_clicked(const QModelIndex &index);
-
+    /**
+     * @brief on_newTripButton_clicked
+     */
     void on_newTripButton_clicked();
-
+    /**
+     * @brief on_startTripButton_clicked
+     */
     void on_startTripButton_clicked();
+    /**
+     * @brief on_StartNormalTrip_clicked
+     */
     void on_StartNormalTrip_clicked();
-
-    //All Recusive algorithm functions
+    /**
+     * @brief TripCreator
+     *
+     * All Recusive algorithm functions
+     *
+     * @param current
+     * @param numOfRest
+     * @param restaurants
+     * @param totalMiles
+     */
     void TripCreator(Restaurant current,
                      int numOfRest,
                      QVector<Restaurant> restaurants,
                      double &totalMiles);
+    /**
+     * @brief FindNextRestaurant
+     * @param current
+     * @param totalMiles
+     * @param IDs
+     * @return int
+     */
     int FindNextRestaurant(Restaurant current,
                             double &totalMiles,
                            std::vector<int> IDs);
+    /**
+     * @brief LoadIDs
+     * @param list
+     * @return vector
+     */
     std::vector<int> LoadIDs(QVector<Restaurant> list);
+    /**
+     * @brief convertIDToRest
+     * @param ID
+     * @param list
+     * @return Restaurant
+     */
     Restaurant convertIDToRest(int ID, QVector<Restaurant> list);
+    /**
+     * @brief PrintOrder
+     */
     void PrintOrder();
+    /**
+     * @brief populateStartLocCB
+     */
     void populateStartLocCB();
+    /**
+     * @brief findClosestRestToSB
+     * @return
+     */
     Restaurant findClosestRestToSB();
-    //to add: function to find starting location from saddleback
+    /**
+     * @brief stringToRest
+     * @param name
+     * @return Restaurant
+     */
     Restaurant stringToRest(QString name);
+    /**
+     * @brief on_restaurantListWidget_2_itemActivated
+     * @param item
+     */
     void on_restaurantListWidget_2_itemActivated(QListWidgetItem *item);
-
+    /**
+     * @brief on_addItemButton_2_clicked
+     */
     void on_addItemButton_2_clicked();
-
+    /**
+     * @brief on_removeItemButton_2_clicked
+     */
     void on_removeItemButton_2_clicked();
-
+    /**
+     * @brief on_restaurantListWidget_2_activated
+     * @param index
+     */
     void on_restaurantListWidget_2_activated(const QModelIndex &index);
-
-   // void on_restaurantListWidget_activated(const QModelIndex &index);
 
 private:
     Ui::MainWindow *ui;
